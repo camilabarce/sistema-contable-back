fd946f4988c553678837fdebe715ec23669666d7
Camila Arce <arce.b.camila@gmail.com>
2024-08-14 17:41:43 -0300

    chore: update database connection configuration

--- src/Database/conexion.js
+++ src/Database/conexion.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const mysql = require('mysql');
 
 const host = process.env.HOST_DB
+const port = process.env.PORT_DB || 3306
 const user = process.env.USER_DB
 const pass = process.env.PASS_DB
 const db = process.env.DATABASE
 
 const conexion = mysql.createConnection({
     host: host,
+    port: port,
     user: user,
     password: pass,
     database: db
